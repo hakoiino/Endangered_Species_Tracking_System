f3114dead40c82bd2f333cd7ff327dcc7ed14ef0
hakoiino <112511620+hakoiino@users.noreply.github.com>
2024-06-11 01:45:11 +0900

    habitat tab works; Threat.java added

---
+++ src/main/java/org/example/Threat.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Threat {
+    private int id;
+    private String name;
+    private String description;
+    private String category;
+    private int habitatId;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getHabitatId() {
+        return habitatId;
+    }
+
+    public void setHabitatId(int habitatId) {
+        this.habitatId = habitatId;
+    }
+}
